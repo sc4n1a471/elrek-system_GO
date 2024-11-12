4c9751bf6d20b25b02d7c583d35acaa47a52b3a7
Martin Terhes <martin.terhes@icloud.com>
2024-11-12 01:51:29 +0100

    feat: added terraform files

---
+++ main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+locals {
+  container_name = var.env == "prod" ? "elrek-system_go_prod" : "elrek-system_go_dev"
+  port_mapping   = var.env == "prod" ? "3000:3000" : "3001:3000"
+}
+
+resource "docker_image" "elrek_system_go" {
+  name = "sc4n1a471/elrek-system_go:${var.container_version}"
+}
+
+resource "docker_container" "elrek_system_go" {
+  name  = local.container_name
+  image = docker_image.elrek_system_go.name
+
+  volumes {
+    host_path      = "/var/log/elrek-system_go"
+    container_path = "/app/logs"
+  }
+
+  env = [
+    "DB_USERNAME=${var.db_username}",
+    "DB_PASSWORD=${var.db_password}",
+    "DB_HOST=${var.db_host}",
+    "DB_PORT=${var.db_port}",
+    "DB_NAME=${var.db_name}",
+    "FRONTEND_URL=${var.frontend_url}",
+    "BACKEND_URL=${var.backend_url}",
+    "DOMAIN=${var.domain}",
+  ]
+
+  ports {
+    internal = 3000
+    external = var.env == "prod" ? 3000 : 3001
+  }
+
+  restart = "on-failure"
+}
---
+++ variables.tf
@@ -0,0 +1,59 @@
+variable "container_version" {
+  description = "Version of the Docker image"
+  type        = string
+  default     = "latest-dev"
+}
+
+variable "env" {
+  description = "Environment to deploy (prod or dev)"
+  type        = string
+  default     = "dev"
+}
+
+variable "db_username" {
+  description = "The database username"
+  type        = string
+  sensitive = true
+}
+
+variable "db_password" {
+  description = "The database password"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_host" {
+  description = "The database host"
+  type        = string
+  sensitive = true
+}
+
+variable "db_port" {
+  description = "The database port"
+  type        = string
+  sensitive = true
+}
+
+variable "db_name" {
+  description = "The database name"
+  type        = string
+  sensitive = true
+}
+
+variable "domain" {
+  description = "The domain name"
+  type        = string
+  sensitive = true
+}
+
+variable "frontend_url" {
+  description = "The frontend URL"
+  type        = string
+  sensitive = true
+}
+
+variable "backend_url" {
+  description = "The backend URL"
+  type        = string
+  sensitive = true
+}
